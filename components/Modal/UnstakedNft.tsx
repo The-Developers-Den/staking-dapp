63f3911794f5672ed52ded7b0578490dbba65fd4
Pratham <prathamsinghal2003@gmail.com>
2023-03-15 18:55:42 +0530

    :bug: Fixed nft index bug

--- components/Modal/UnstakedNft.tsx
+++ components/Modal/UnstakedNft.tsx
@@ -6,7 +6,9 @@ import { useAccount, useContract, useSigner } from "wagmi";
 const UnstakedNft = () => {
   const { data: signer } = useSigner();
   const { address } = useAccount();
-  const [nfts, setNfts] = React.useState<string[]>([]);
+  const [nfts, setNfts] = React.useState<{ tokenId: number; url: string }[]>(
+    []
+  );
   const nftContract = useContract({
     address: NFTAbi.address,
     abi: NFTAbi.abi,
@@ -22,7 +24,7 @@ const UnstakedNft = () => {
           for (let i = 0; i < index; i++) {
             const tx = await nftContract?.tokenOfOwnerByIndex(address, i);
             const tx2 = await nftContract?.tokenURI(tx.toNumber());
-            setNfts((prev) => [...prev, tx2]);
+            setNfts((prev) => [...prev, { tokenId: tx.toNumber(), url: tx2 }]);
           }
         } catch (err) {
           console.log(err);
@@ -34,10 +36,10 @@ const UnstakedNft = () => {
 
   return (
     <div className="flex flex-col mx-auto text-center">
-      <h2 className="text-2xl">Your UnStaked NFTs</h2>
+      <h2 className="text-2xl">Your NFTs</h2>
       <div className="flex mx-auto my-7">
         {nfts.map((nft, id) => (
-          <NFTCard key={id} url={nft} stake={true} tokenId={id} />
+          <NFTCard key={id} url={nft.url} stake={true} tokenId={nft.tokenId} />
         ))}
       </div>
     </div>
